7d37217dffa37604ec7662fa4623b7c34b462ed9
Buildeployship <197860382+Buildeployship@users.noreply.github.com>
2025-12-12 18:12:18 -0800

    speedinsights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
+
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
